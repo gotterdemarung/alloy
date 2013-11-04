597e248420110c529939b34c6b15a934ebf62d17
gotterdemarung <gotterdemarung@gmail.com>
2013-11-04 22:31:50 +0200

    cache implementation

--- src/Alloy/Net/HTTPClient.php
+++ src/Alloy/Net/HTTPClient.php
@@ -19,6 +19,44 @@ class HTTPClient implements IObservable
     private $_observers = array();
 
     /**
+     * Helper function for debug-level logging
+     *
+     * @param mixed $message
+     */
+    protected function _debug($message)
+    {
+        if (count($this->_observers) === 0) {
+            return;
+        }
+        $this->notify(
+            new Packet(
+                $message,
+                'HTTPClient',
+                Packet::LEVEL_DEBUG
+            )
+        );
+    }
+
+    /**
+     * Helper function for notice-level logging
+     *
+     * @param mixed $message
+     */
+    protected function _notice($message)
+    {
+        if (count($this->_observers) === 0) {
+            return;
+        }
+        $this->notify(
+            new Packet(
+                $message,
+                'HTTPClient',
+                Packet::LEVEL_NOTICE
+            )
+        );
+    }
+
+    /**
      * Sets cache
      *
      * @param array|\ArrayAccess $cache
@@ -41,13 +79,7 @@ class HTTPClient implements IObservable
         $this->_cache = $cache;
 
         // Logging
-        $this->notify(
-            new Packet(
-                'Setting ' . get_class($this->_cache) . ' as cache',
-                'HTTPClient',
-                Packet::LEVEL_DEBUG
-            )
-        );
+        $this->_debug('Setting ' . get_class($this->_cache) . ' as cache');
     }
 
     /**
@@ -82,27 +114,41 @@ class HTTPClient implements IObservable
      */
     public function get($url, $requestOptions = null)
     {
+        // Checking cache
+        if ($this->_cache !== null && ($cached = $this->_cache[$url]) !== false) {
+            // Cache hit
+            $this->_notice("Cache hit on {$url}");
+            $this->_notice('Received ' . strlen($cached) . ' bytes');
+            return $cached;
+        }
+
         // Logging
-        $this->notify(
-            new Packet(
-                "Preparing request to {$url}",
-                'HTTPClient',
-                Packet::LEVEL_NOTICE
-            )
-        );
+        $this->_notice("Preparing request to {$url}");
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $answer = curl_exec($curl);
 
+        if ($answer === false) {
+            // curl error
+            $this->notify(
+                new Packet(
+                    "No data received from {$url}",
+                    'HTTPClient',
+                    Packet::LEVEL_ERROR
+                )
+            );
+            throw new \Exception("No data received from {$url}");
+        }
+
         // Logging
-        $this->notify(
-            new Packet(
-                'Received ' . strlen($answer) . ' bytes',
-                'HTTPClient',
-                Packet::LEVEL_NOTICE
-            )
-        );
+        $this->_notice('Received ' . strlen($answer) . ' bytes');
+
+        // Putting into cache
+        if ($this->_cache !== null) {
+            $this->_cache[$url] = $answer;
+            $this->_notice("Stored in cache");
+        }
 
         return $answer;
     }
