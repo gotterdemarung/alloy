e2bb36b1385398b2eff2cd5ed3a9c1f36c0b729a
gotterdemarung <gotterdemarung@gmail.com>
2013-11-04 22:39:16 +0200

    utility functions

--- src/Alloy/Observers/Observable.php
+++ src/Alloy/Observers/Observable.php
@@ -22,6 +22,21 @@ abstract class Observable implements IObservable
     }
 
     /**
+     * Returns tag for current object
+     *
+     * @return string
+     */
+    protected function _getObservableTag()
+    {
+        $tag = get_class($this);
+        if (strpos($tag, '\\') !== false) {
+            $tag = explode('\\', $tag);
+            $tag = $tag[count($tag) - 1];
+        }
+        return $tag;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function notify(Packet $packet)
@@ -36,19 +51,70 @@ abstract class Observable implements IObservable
     }
 
     /**
+     * Register an exception and rethrows it
+     *
+     * @param \Exception $exception
+     * @param string $tag
+     * @throws \Exception
+     */
+    protected function _oRethrow($exception, $tag = null) {
+        if (count($this->_observers) === 0) {
+            throw $exception;
+        }
+        if ($tag === null) {
+            $tag = $this->_getObservableTag();
+        }
+        $this->notify(new Packet($exception, $tag, Packet::LEVEL_ERROR));
+        throw $exception;
+    }
+
+    /**
+     * Sends message to observers, using WARNING level
+     *
+     * @param mixed  $message
+     * @param string $tag
+     */
+    protected function _oWarn($message, $tag = null)
+    {
+        if (count($this->_observers) === 0) {
+            return;
+        }
+        if ($tag === null) {
+            $tag = $this->_getObservableTag();
+        }
+        $this->notify(new Packet($message, $tag, Packet::LEVEL_WARNING));
+    }
+
+    /**
+     * Sends message to observers, using DEBUG level
+     *
+     * @param mixed  $message
+     * @param string $tag
+     */
+    protected function _oDebug($message, $tag = null)
+    {
+        if (count($this->_observers) === 0) {
+            return;
+        }
+        if ($tag === null) {
+            $tag = $this->_getObservableTag();
+        }
+        $this->notify(new Packet($message, $tag, Packet::LEVEL_DEBUG));
+    }
+
+    /**
      * Sends message to observers, using NOTICE level
      *
      * @param mixed  $message
      * @param string $tag
      */
-    public function log($message, $tag = null)
+    protected function _oLog($message, $tag = null)
     {
+        if (count($this->_observers) === 0) {
+            return;
+        }
         if ($tag === null) {
-            $tag = get_class($this);
-            if (strpos($tag, '\\') !== false) {
-                $tag = explode('\\', $tag);
-                $tag = $tag[count($tag) - 1];
-            }
+            $tag = $this->_getObservableTag();
         }
         $this->notify(new Packet($message, $tag, Packet::LEVEL_NOTICE));
     }
